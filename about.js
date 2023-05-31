22c3920387a749a49c1b953fe19644a4230bade0
Natalia <zadepschi@yahoo.com>
2023-05-31 22:09:51 +0300

    added animation

---
+++ about.js
@@ -0,0 +1,5 @@
+gsap.from(".par", {y: -20, opacity: 0, duration: 3, delay: .5})
+gsap.from("h1", {y: -20, opacity: 0, duration: 3, delay: .5})
+
+gsap.from(".me", {x: -100, opacity: 0, duration: 3, delay: .5})
+gsap.from(".computer", {x: 100, opacity: 0, duration: 3, delay: .5})
